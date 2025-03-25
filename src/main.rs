a2124f7e8627f78085d94c08157dde45ad4d4638
itsMugen <77022426+itsMugen@users.noreply.github.com>
2025-03-25 23:04:53 +0100

    not even sure mate

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use axum::{
 use rand::prelude::*;
 use serde::Deserialize;
 use serde_json::Value;
+use std::collections::HashMap;
 use std::fs;
 use std::sync::Arc;
 use tera::{Context, Tera};
@@ -25,32 +26,9 @@ struct Stats {
     spellblock: u16,
 }
 
-#[derive(Deserialize, Debug, Clone)]
-struct Champion {
-    name: String,
-    stats: Stats,
-}
-
-impl Champion {
-    fn get(&self, field: &str) -> f32{
-        match field {
-            "armor" => f32::from(self.stats.armor),
-            "attackrange" => f32::from(self.stats.attackrange),
-            "attackdamage" => f32::from(self.stats.attackdamage),
-            "attackspeed" => f32::from(self.stats.attackspeed),
-            "hp" => f32::from(self.stats.hp),
-            "hpregen" => f32::from(self.stats.hpregen),
-            "movespeed" => f32::from(self.stats.movespeed),
-            "mp" => f32::from(self.stats.mp),
-            "spellblock" => f32::from(self.stats.spellblock),
-            _ => 0.0,
-        }
-    }
-}
-
 struct AppState {
     templates: Tera,
-    champion_list: Vec<Champion>,
+    champion_list: HashMap<String, Stats>,
 }
 
 #[tokio::main]
@@ -68,10 +46,9 @@ async fn main() {
         .route("/stat_check", get(stat_check))
         .route("/check_stats", post(check_stat))
         .nest_service("/styles", ServeDir::new("src/frontend/styles"))
-        .nest_service(
-            "/champ_images",
-            ServeDir::new("attic/img/champion/centered"),
-        )
+        .nest_service("/champ_images", ServeDir::new("attic/img/champion/splash"))
+        .nest_service("/stats", ServeDir::new("attic/img/perk-images/StatMods"))
+        .nest_service("/items", ServeDir::new("attic/15.5.1/img/item"))
         .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
@@ -85,43 +62,34 @@ async fn check_stat(State(_state): State<Arc<AppState>>) -> Html<String> {
 
 async fn stat_check(State(state): State<Arc<AppState>>) -> Html<String> {
     //pick 2 random champs
-    let mut champs_list = state.champion_list.clone();
+    let champ_map = state.champion_list.clone();
     let mut rng = rand::rng();
-    champs_list.shuffle(&mut rng);
-    let mut champs = champs_list.choose_multiple(&mut rng, 2);
+    let mut champ_names: Vec<String> = champ_map.into_keys().collect();
+    champ_names.shuffle(&mut rng);
+    let mut champs = champ_names.choose_multiple(&mut rng, 2);
     let champ_1 = champs.next();
     let champ_2 = champs.next();
 
-    //pick a stat
-    let mut stat_rng = rand::rng();
-    let random_stat = match stat_rng.gen_range(0..8) {
-        0 => "armor",
-        1 => "attackrange",
-        2 => "attackdamage",
-        3 => "attackspeed",
-        4 => "hp",
-        5 => "hpregen",
-        6 => "movespeed",
-        7 => "mp",
-        8 => "spellblock",
-        _ => "nada",
-    };
-
     //create page
     let mut context = Context::new();
-    context.insert("champion_name_1", &champ_1.unwrap().name);
-    context.insert("champion_name_2", &champ_2.unwrap().name);
-    context.insert("stat", &random_stat);
-
-    println!("{:#?}", &champ_2.unwrap().get(&random_stat));
+    if champ_1.unwrap() == "Fiddlesticks" {
+        context.insert("champion_name_1", "FiddleSticks");
+        context.insert("champion_name_2", &champ_2.unwrap());
+    } else if champ_2.unwrap() == "Fiddlesticks" {
+        context.insert("champion_name_1", &champ_1.unwrap());
+        context.insert("champion_name_2", "FiddleSticks");
+    } else {
+        context.insert("champion_name_1", &champ_1.unwrap());
+        context.insert("champion_name_2", &champ_2.unwrap());
+    }
 
     let rendered = state.templates.render("base.html", &context).unwrap();
     Html(rendered)
 }
 
-fn aggregate_data() -> Vec<Champion> {
+fn aggregate_data() -> HashMap<String, Stats> {
     let champs_dir = fs::read_dir("attic/15.5.1/data/en_US/champion").unwrap();
-    let mut champs: Vec<Champion> = Vec::new();
+    let mut champs: HashMap<String, Stats> = HashMap::new();
     for champ_path in champs_dir {
         let path = champ_path.unwrap();
         let champ_file = &path.file_name().into_string().unwrap();
@@ -134,12 +102,7 @@ fn aggregate_data() -> Vec<Champion> {
         let stats: Stats =
             serde_json::from_value(champ_data["data"][champ_name]["stats"].clone()).unwrap();
 
-        let champion: Champion = Champion {
-            name: champ_name.to_string(),
-            stats: stats,
-        };
-
-        champs.push(champion);
+        champs.insert(champ_name.to_string(), stats);
     }
     champs
 }
